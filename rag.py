cfd8e7079c535be4c0bc22c0304f76a3adfea298
Krisna Dibyo <krisna_itbstei@hotmail.com>
2025-04-02 16:13:26 +0700

    download caption first before download the audio and transcribe it

--- rag.py
+++ rag.py
@@ -28,42 +28,10 @@ def download_video(url: str, video_id: str):
   if not os.path.exists(output_path):
     os.makedirs(output_path)
   yt.streams.filter(only_audio=True).first().download(output_path=output_path, filename=file_name)
-  return f"{output_path}/{file_name}"
 
-def download_captions(url: str, video_id: str):
-  print(f"Downloading captions...")
-  yt = YouTube(url)
-  caption = yt.captions.get_by_language_code('en')
-  if caption is None:
-    caption = yt.captions.get_by_language_code('id')
-  caption_text = caption.generate_srt_captions()
-  caption_text = re.sub(r'\d+\n', '', caption_text)
-  # remove the timestamps
-  caption_text = re.sub(r'\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},', '', caption_text)
-  caption_text = re.sub(r'\n', ' ', caption_text)
-  
-  with open(f"transcripts/{video_id}.txt", "w") as f:
-    f.write(caption_text)
-  return f"transcripts/{video_id}.txt"
-
-# def get_chat_model():
-#   if not os.environ.get("OPENAI_API_KEY"):
-#     raise ValueError("OPENAI_API_KEY is not set")
-#   llm = init_chat_model(model="gpt-4o-mini", model_provider="openai")
-#   return llm
-
-# def get_embeddings():
-#   if not os.environ.get("OPENAI_API_KEY"):
-#     raise ValueError("OPENAI_API_KEY is not set")
-#   embeddings = OpenAIEmbeddings(model="text-embedding-3-large")
-#   return embeddings
-
-# def get_vector_store(embeddings):
-#   vector_store = InMemoryVectorStore(embeddings)
-#   return vector_store
-
-def transcribe_video(file_path: str):
+def transcribe_video(video_id: str):
   print(f"Transcribing video...")
+  file_path = f"audio/{video_id}.mp3"
   model = whisper.load_model("tiny", device="cpu")
   result = model.transcribe(file_path)
   text = result["text"]
@@ -75,29 +43,56 @@ def transcribe_video(file_path: str):
     f.write(text)
   # delete the audio file
   os.remove(file_path)
-  return f"{output_path}/{file_name}.txt"
 
-def load_transcript(file_path: str):
-  loader = TextLoader(file_path)
-  docs = loader.load()
-  return docs
+def download_captions(url: str, video_id: str):
+  print(f"Downloading captions...")
+  yt = YouTube(url)
+  print(yt.captions)
+  caption = yt.captions.get_by_language_code('en')
+  if caption is None:
+    caption = yt.captions.get_by_language_code('a.en')
+  if caption is None:
+    caption = yt.captions.get_by_language_code('a.id')
+  if caption is None:
+    # fallback to download video with audio
+    download_video(url, video_id)
+    transcribe_video(video_id)
+    return
+  caption_text = caption.generate_srt_captions()
+  caption_text = re.sub(r'\d+\n', '', caption_text)
+  # remove the timestamps
+  caption_text = re.sub(r'\d{2}:\d{2}:\d{2},\d{3} --> \d{2}:\d{2}:\d{2},', '', caption_text)
+  caption_text = re.sub(r'\n', ' ', caption_text)
   
-def split_transcript(transcript: str, video_id: str):
+  with open(f"transcripts/{video_id}.txt", "w") as f:
+    f.write(caption_text)
+  
+def split_transcript(video_id: str):
+  loader = TextLoader(f"transcripts/{video_id}.txt")
+  docs = loader.load()
   text_splitter = RecursiveCharacterTextSplitter(chunk_size=500, chunk_overlap=150)
-  all_splits = text_splitter.split_documents(transcript)
+  all_splits = text_splitter.split_documents(docs)
   print(len(all_splits))
+  url = f"https://www.youtube.com/watch?v={video_id}"
+  yt = YouTube(url)
+  # adding metadata to the documents
   for doc in all_splits:
-    doc.metadata.update({"video_id": video_id})
+    doc.metadata.update({
+      "video_id": video_id,
+      "video_author": yt.author,
+      "video_title": yt.title,
+      "video_description": yt.description,
+    })
   return all_splits
 
-def retreive(vector_store, question: str, video_id: str):
+def retrieve(vector_store, question: str, video_id: str):
   def _filter_document(doc: Document) -> bool:
     return doc.metadata.get("video_id", None) == video_id
-  retrieve_docs = vector_store.similarity_search(question, k= 1, filter=_filter_document)
+  retrieve_docs = vector_store.similarity_search(question, k= 3, filter=_filter_document)
   print(len(retrieve_docs))
   return retrieve_docs
 
-def generate_answer(llm, question: str, docs: List[Document]):
+def generate_answer(question: str, docs: List[Document]):
   docs_content = "\n\n".join(doc.page_content for doc in docs)
   prompt = hub.pull("rlm/rag-prompt")
   prompt = prompt.invoke({"question": question, "context": docs_content})
@@ -105,15 +100,13 @@ def generate_answer(llm, question: str, docs: List[Document]):
   return answer.content
 
 def ingest_video(url_video: str, video_id: str):
-  file_name = download_video(url_video, video_id)
-  transcript = transcribe_video(file_name)
-  transcript = load_transcript(transcript)
-  all_splits = split_transcript(transcript, video_id)
+  download_captions(url_video, video_id)
+  all_splits = split_transcript(video_id)
   vector_store.add_documents(all_splits)
 
 def answer_question(question: str, video_id: str):
-  retrieve_docs = retreive(vector_store, question, video_id)
-  response = generate_answer(llm, question, retrieve_docs)
+  retrieve_docs = retrieve(vector_store, question, video_id)
+  response = generate_answer(question, retrieve_docs)
   return response
 
 # def rag_video(question: str, url_video: str):
@@ -131,30 +124,11 @@ if __name__ == "__main__":
   input_url = input("Enter the URL of the video to download: ")
   download_captions(input_url, "2")
 
+  all_splits = split_transcript("2")
+  vector_store.add_documents(all_splits)
+  question = ""
 
-  # question = input("Enter a question: ")
-
-  # # download and transcribe the video
-  # file_name = transcribe_video(download_video(input_url))
-
-  # # load the transcript
-  # transcript = load_transcript(file_name)
-
-  # # split the transcript into chunks
-  # print(f"Splitting transcript into chunks...")
-  # all_splits = split_transcript(transcript)
-
-  # # Index the chunks
-  # print(f"Indexing chunks...")
-  # vector_store.add_documents(all_splits)
-
-
-  # # retrieve the most relevant documents
-  # question = question + " -- This is a question from a video transcript as RAG context"
-  # print(f"Retrieving most relevant documents...")
-  # retrieve_docs = retreive(vector_store, question)
-
-  # # generate the answer
-  # print(f"Generating answer...")
-  # response = generate_answer(llm, question, retrieve_docs)
-  # print("Answer: ", response)
+  while question != "exit":
+    question = input("Enter a question: ")
+    response = answer_question(question, "2")
+    print(response)
